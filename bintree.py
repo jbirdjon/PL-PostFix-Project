ed057ec9e07a8d135de4e43d7281664fed8c5595
Jon Wallis <jbirdjon@gmail.com>
2020-09-13 19:58:15 -0500

    Update bintree.py

--- bintree.py
+++ bintree.py
@@ -13,23 +13,14 @@ def bin(n):
                     yield (l,r)
     file1.close()
 
-def createtree():
-    with open('postfix.txt') as pos:
-        while True:
-            character = pos.read(1)
-            if not character: # program complete
-                break
-            print(character + " ")
-
-def file2string(fname) :
-    with open(fname, 'r') as f:
-        return f.read()
-
-def string2file(string,fname) :
-    with open(fname,'w') as g:
-        g.write(string)
-
 file1 = open('postfix.txt',"w")
 
-for t in bin(3): file1.write(str(t)+"\n")
-createtree()
+for t in bin(3):
+    postfix = ""
+    for element in str(t):
+        if element == 'o':
+            postfix = postfix + 'o'
+        elif element == ')':
+            postfix = postfix + ','
+
+    file1.write(postfix+"\n")
