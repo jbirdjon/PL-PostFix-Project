1d716016ed285aeea2e6f4212b76f5f08847836f
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-09-13 19:48:50 -0500

    i

--- bintree.py
+++ bintree.py
@@ -3,13 +3,11 @@ def bin(n):
     file1 = open('postfix.txt', "w")#open file
 
     if n==0:
-        file1.write('o')
         yield 'o'
     else:
         for k in range(0,n):
             for l in bin(k):
                 for r in bin(n-1-k):
-                    file1.write('x')
                     yield (l,r)
     file1.close()
 
@@ -31,5 +29,7 @@ def string2file(string,fname) :
 
 file1 = open('postfix.txt',"w")
 
-for t in bin(3): file1.write(str(t)+"\n")
+for t in bin(3): 
+    file1.write(str(t)+"\n")
+    print(str(t))
 createtree()
