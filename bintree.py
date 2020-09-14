c934a74f25394413c90f6d01ad3408ba4c35d64d
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-09-13 21:07:55 -0500

    p

--- bintree.py
+++ bintree.py
@@ -13,7 +13,7 @@ def bin(n):
 
 file1 = open('postfix.txt',"w")
 
-for t in bin(3):
+for t in bin(2):
 
     postfix = ""
     for element in str(t):
@@ -23,4 +23,18 @@ for t in bin(3):
             postfix = postfix + ','
 
     file1.write(postfix+"\n")
+file1.close()
+f = open("postfix.txt","r")
+for line in f.readlines():
+    stack = []
+    if line != '':
+        for character in line:
+            print(character)
+            if character != '\n':
+                stack.append(character)
+            if character == ',':
+                print("Top of stack")
+                print(stack.pop())
+    print(stack)
 
+f.close()
