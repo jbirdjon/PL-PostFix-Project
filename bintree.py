b70e3c46a442671c63a19ec2190ed101f29f26c9
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-09-17 18:11:48 -0500

    Update bintree.py

--- bintree.py
+++ bintree.py
@@ -1,7 +1,5 @@
-
+#Creates binary tree
 def bin(n):
-    file1 = open('postfix.txt', "w")#open file
-
     if n==0:
         yield 'o'
     else:
@@ -9,10 +7,10 @@ def bin(n):
             for l in bin(k):
                 for r in bin(n-1-k):
                     yield (l,r)
-    file1.close()
 
 file1 = open('postfix.txt',"w")
 
+#Writing to file
 for t in bin(3):
 
     postfix = ""
@@ -25,18 +23,20 @@ for t in bin(3):
     file1.write(postfix+"\n")
     print(str(t)+"\t"+postfix)
 file1.close()
+
+#reading from file
 f = open("postfix.txt","r")
 for line in f.readlines():
     stack = []
     stack2 = []
     combo =""
     if line != '':
-        for character in line:
-            if character != '\n' and character !=',':
+        for character in line:#reading characters in line
+            if character != '\n' and character !=',':#appends character to stack as long as its not delimiter
                 stack.append(character)
-            elif character == ',':
+            elif character == ',':#Creates a combo and appends it to the stack
                 pos=stack.pop()
                 pos2=stack.pop()
                 stack.append("("+pos2+","+pos+")")
-    print(str(stack)+"\t"+line)
+    print(str(stack)+"\t"+line)#Verifys
 f.close()
